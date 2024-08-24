416bf73454701c1a5a58ceea623ad6c38af146ac
Oksana_Cherviachenkoclear <kvintinovna@gmail.com>
2024-08-24 22:49:32 +0200

    done task-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,46 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = {
+  input: document.querySelector('#controls input'),
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+  boxesContainer: document.querySelector('#boxes'),
+};
+
+controls.createBtn.addEventListener('click', () => {
+  const amount = parseInt(controls.input.value);
+  
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    controls.input.value = ''; 
+  }
+});
+
+controls.destroyBtn.addEventListener('click', () => {
+  destroyBoxes();
+  controls.input.value = ''; 
+});
+
+function createBoxes(amount) {
+  destroyBoxes(); 
+
+  const elements = [];
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement('div');
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    size += 10;
+    elements.push(div);
+  }
+
+  controls.boxesContainer.append(...elements);
+}
+
+function destroyBoxes() {
+  controls.boxesContainer.innerHTML = ''; 
+}
